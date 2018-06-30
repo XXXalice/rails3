1d1852394ef568abde8d6696cb2b559060e0b7a9
梅宮 <umemiya@MacBook-Pro.local>
2018-07-01 01:25:23 +0900

    fix application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  def hellomo
-    render html: "helllllo"
-=======
   def hello
     render html: "hellllo"
->>>>>>> sign-up
   end
 end
