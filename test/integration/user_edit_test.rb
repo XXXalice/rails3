5224ad47b33024a8cb828d8a0dfa208c6053e792
梅宮 <umemiya@MacBook-Pro.local>
2018-07-10 17:16:37 +0900

    all complete user_edit_test

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -9,14 +9,21 @@ class UserEditTest < ActionDispatch::IntegrationTest
   test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
+    name = "fooooo"
+    email = "foo@bar.com"
     patch user_path(@user), params: {
         user: {
-            name: "",
-            email: "foo@bar.com",
-            password: "foo",
-            password_confirmaiton: "bar"
+            name: name,
+            email: email,
+            password: "",
+            password_confirmaiton: ""
         }
     }
-    assert_template 'users/edit'
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+
   end
 end
