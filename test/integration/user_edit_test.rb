5015c2070a3ab112dc48ccfd31abb6927d4352ea
梅宮 <umemiya@MacBook-Pro.local>
2018-07-10 18:07:13 +0900

    add cant edit & update when wrong user logged in

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class UserEditTest < ActionDispatch::IntegrationTest
 
+  attr_reader :user
+
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "fooooo"
@@ -24,6 +28,25 @@ class UserEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
-
   end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: {
+        user: {
+            name: @user.name,
+            email: @user.email
+        }
+    }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
 end
